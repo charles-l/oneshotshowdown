08c9ccaa15f341860ad1fbfa6a9b59a5b84f5f19
Charles S <chuckiels2011@gmail.com>
2013-12-14 12:45:54 -0500

    ADDED MORE STUFF

--- main.lua
+++ main.lua
@@ -1,11 +1,23 @@
 function love.load()
 		world = love.physics.newWorld(0, 32, false)
+		world:setCallbacks( beginContact, endContact, preSolve, postSolve )
 		e = {}
 		e.f = {}
 		e.f.b = love.physics.newBody(world, 0, 0)
 		e.f.s = love.physics.newEdgeShape(0, love.graphics.getHeight(), love.graphics.getWidth(), love.graphics.getHeight())
 		e.f.f = love.physics.newFixture(e.f.b, e.f.s)
-		e.f.f:setRestitution(1)
+		e.r = {}
+		e.r.b = love.physics.newBody(world, 0, 0)
+		e.r.s = love.physics.newEdgeShape(love.graphics.getWidth(), 0, love.graphics.getWidth(), love.graphics.getHeight())
+		e.r.f = love.physics.newFixture(e.r.b, e.r.s)
+		e.t = {}
+		e.t.b = love.physics.newBody(world, 0, 0)
+		e.t.s = love.physics.newEdgeShape(0, 0, love.graphics.getWidth(), 0)
+		e.t.f = love.physics.newFixture(e.t.b, e.t.s)
+		e.l = {}
+		e.l.b = love.physics.newBody(world, 0, 0)
+		e.l.s = love.physics.newEdgeShape(0, 0, 0, love.graphics.getHeight())
+		e.l.f = love.physics.newFixture(e.l.b, e.l.s)
 		fire = false
 		guy = love.graphics.newImage('guy.png')
 		guy:setFilter("nearest", "nearest")
@@ -14,6 +26,9 @@ function love.load()
 		up = love.graphics.newQuad(16, 0, 16, 16, 48, 16)
 		down = love.graphics.newQuad(32, 0, 16, 16, 48, 16)
 		dir = flat
+		rockimg = love.graphics.newImage("rock.png")
+		rockimg:setFilter("nearest", "nearest")
+		ricochet = love.audio.newSource("ricochet.wav", "static")
 end
 function spawnBullet(x, y, tx, ty)
 		if not fire then
@@ -22,6 +37,8 @@ function spawnBullet(x, y, tx, ty)
 				bullet.b = love.physics.newBody(world, x, y, "dynamic")
 				bullet.s = love.physics.newCircleShape(5)
 				bullet.f = love.physics.newFixture(bullet.b, bullet.s)
+				bullet.f:setRestitution(1)
+				bullet.f:setUserData("bullet")
 				bullet.b:setMass(10)
 				ang = math.atan2((ty-y), (tx-x))
 				local tx = math.cos(ang) * 1*math.pow(10, 5.5)
@@ -62,9 +79,20 @@ function love.update(dt)
 		end
 end
 function love.draw()
+		love.graphics.reset()
 		love.graphics.setBackgroundColor(255, 255, 255)
+		love.graphics.draw(rockimg, 0, love.graphics.getHeight()/2 + 14, 0, 4)
 		love.graphics.draw(guy, dir, 100, love.graphics.getHeight()/2, 0, 4, 4, guy:getWidth()/2, guy:getHeight()/2)
 		if(fire) then
 				love.graphics.draw(bulletimg, bullet.b:getX(), bullet.b:getY(), ang, 4)
+		end
+		love.graphics.setColor(0, 0, 0)
+		love.graphics.print("FPS " .. love.timer.getFPS(), 10, 10)
+end
+function beginContact(a, b, col)
+		if(a:getUserData() == "bullet" or b:getUserData() == "bullet") then
+				love.audio.rewind(ricochet)
+				ricochet:setPitch( math.random(1, 5) )
+				love.audio.play(ricochet)
 		end
 end
